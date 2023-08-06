162f92f07f472e0927a44f1008f9e89e4a8a9b58
vladernn <vladys.3234@gmail.com>
2023-08-06 12:16:14 +0200

    add exchange rate gateway

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExchangeRateGateway } from './exchange-rate/exchange-rate.gateway';
+import { ExchangeRateGateway } from './home/exchange-rate/exchange-rate.gateway';
+import { AccountsController } from './home/accounts/aplication/accounts/accounts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AccountsController],
   providers: [AppService, ExchangeRateGateway],
 })
 export class AppModule {}
