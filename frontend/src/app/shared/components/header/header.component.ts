0f9b77e2ce7c767ba8f7d45c374f6e8dd1dbeb80
vladernn <vladys.3234@gmail.com>
2025-01-29 20:19:40 +0100

    feat: enhance header component to highlight BTC price changes with color indicators

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -4,13 +4,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { BehaviorSubject } from 'rxjs';
+import { HighlightChangeDirective } from '@shared/directives/highlight-change-directive/highlight-change.directive';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
     standalone: true,
-    imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule]
+    imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, HighlightChangeDirective]
 })
 export class HeaderComponent {
     @Input() btcPrice: BehaviorSubject<number> = new BehaviorSubject<number>(0);
